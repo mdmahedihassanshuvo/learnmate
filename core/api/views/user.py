36e399afe3cb3c447bba3f00823c641258f0a758
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-05 23:50:51 +0600

    Update user API views

--- core/api/views/user.py
+++ core/api/views/user.py
@@ -1,9 +1,27 @@
 # REST FRAMEWORK IMPORTS
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # LOCAL IMPORTS
-from core.api.serializer import UserSerializer
+from core.api.serializer import LoginSerializer, SignupSerializer
 
 
-class UserCreateAPIView(CreateAPIView):
-    serializer_class = UserSerializer
+class SignupAPIView(CreateAPIView):
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
+
+
+class LoginAPIView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+
+class CurrentUserAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = SignupSerializer(request.user)
+        return Response(serializer.data)
