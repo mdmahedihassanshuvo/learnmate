3f8b2d17bb4d8cd189d28c6ff76e7dad2d85367f
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-05 23:50:47 +0600

    Update core admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
+# DJANGO IMPORTS
 from django.contrib import admin
 
-# Register your models here.
+# LOCAL IMPORTS
+from .models import User, Profile
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+        'is_superuser'
+    )
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('id',)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'name',
+        'fathers_name',
+    )
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
+    ordering = ('id',)
