17292b2f14ce5376229f762cb6fdb19463b8056c
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-05 23:50:50 +0600

    Update core API URLs

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
-from core.api.views.user import UserCreateAPIView
+from core.api.views import CurrentUserAPIView, LoginAPIView, SignupAPIView
 
 urlpatterns = [
-    path('users/', UserCreateAPIView.as_view(), name='user-create'),
+    path('signup/', SignupAPIView.as_view(), name='signup'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+    path('me/', CurrentUserAPIView.as_view(), name='current-user'),
 ]
