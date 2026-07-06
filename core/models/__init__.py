5b4da7ff7d99bcd2bfcde7e37961f320f0e52e2f
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-06 23:12:36 +0600

    Update models init

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,10 +2,18 @@ from .base_model import BaseModel
 from .site_information import SiteConfig
 from .profile import Profile
 from .user import User
+from .menu import (
+    Menu,
+    MenuSection,
+    DashboardLine
+)
 
 __all__ = [
     'User',
     'Profile',
     'BaseModel',
-    'SiteConfig'
+    'SiteConfig',
+    'Menu',
+    'MenuSection',
+    'DashboardLine'
 ]
