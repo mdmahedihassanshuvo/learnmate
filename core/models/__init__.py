3104f3df8dab5ef4fd75eec83031afac3520b674
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-06 22:55:11 +0600

    Update models init

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,11 @@
-from .common_model import CommonModel
+from .base_model import BaseModel
+from .site_information import SiteConfig
 from .profile import Profile
 from .user import User
 
 __all__ = [
     'User',
     'Profile',
-    'CommonModel',
+    'BaseModel',
+    'SiteConfig'
 ]
