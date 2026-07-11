d551ded8292bf98a428a649ea2bec8583fd63986
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-11 23:21:54 +0600

    Add authentication layout component

---
+++ frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import { Outlet } from 'react-router';
+import { Player } from "@lottiefiles/react-lottie-player";
+import groovyWalkAnimation from "../assets/login.json";
+
+const AuthLayout = () => {
+    return (
+        <div className="flex items-center justify-evenly min-h-screen bg-gray-100">
+            <Outlet />
+            <div>
+                <Player
+                    autoplay
+                    loop
+                    src={groovyWalkAnimation}
+                    style={{ height: "350px", width: "350px" }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default AuthLayout;
