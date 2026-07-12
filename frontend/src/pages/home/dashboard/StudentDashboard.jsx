9d931092bd5482db12cc0585918c0517bca922e9
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-12 22:45:39 +0600

    Update student dashboard

--- frontend/src/pages/home/dashboard/StudentDashboard.jsx
+++ frontend/src/pages/home/dashboard/StudentDashboard.jsx
@@ -1,7 +1,23 @@
+import { Outlet } from "react-router-dom";
+
 const StudentDashboard = () => {
     return (
-        <div>
-            this is student dashboard
+        <div className="drawer lg:drawer-open">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center justify-center">
+                <Outlet />
+                <label htmlFor="my-drawer-3" className="btn drawer-button lg:hidden">
+                Open drawer
+                </label>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
