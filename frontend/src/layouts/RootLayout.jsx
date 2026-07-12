3a7562f86d566171cf4662e1ad226e3409286ecd
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-12 22:45:38 +0600

    Update root layout

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from '../pages/shared/navbar/NavBar';
 
 const RootLayout = () => {
     return (
         <div>
+            <NavBar />
             <Outlet />
         </div>
     );
