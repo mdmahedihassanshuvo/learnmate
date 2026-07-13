b8f028279c7ddb9bef18c3dd5610f1840325dfa9
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-13 11:30:35 +0600

    config file update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig(({ command, mode }) => {
       tailwindcss(),
       react()
     ],
+    build: {
+      outDir: isGhPages ? path.resolve(__dirname, 'dist') : '',
+      emptyOutDir: true
+    },
     server: {
       proxy: {
         '/api': {
