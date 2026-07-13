39b7a8f337ac1b1a6398302da33ee0fdb82cc00e
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-13 11:18:06 +0600

    root changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
   const isGhPages = mode === 'ghpages'
   return {
-    base: isGhPages ? '/' : command === 'build' ? '/learnmate/' : '/',
+    base: isGhPages ? '/' : command === 'build' ? '/' : '/',
     plugins: [
       tailwindcss(),
       react()
